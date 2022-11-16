b071da72d2548a17e2d71b27afe4bea75d3ec46a
Your Name <you@example.com>
2022-11-16 22:02:39 +0300

    Созданы app.js и index.html

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// let a = 5;
+// function number(a, n = 1) {
+//     return for (i=2; i<=n; i++) {
+//         a=a*a;
+//     }    
+// }
+
+function func1(a=3) {
+    return a*a;
+}
+
+console.log(`${func1()}`);
+
